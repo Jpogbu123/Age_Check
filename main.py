608aaab175a0636fc0e6ab10fff89f76b5714722
Joy Ogbu <Jpogbu@gmail.com>
2023-05-15 16:05:10 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("what is your name:")
+name =input()
+print("welcome" + ""+ name)
+print(" enter the age you are turning this year :")
+age=int(input())
+print("in the next 20 years you will be this age :")
+num= int (age+20)
+print(num)
+print("enter current year")
+a=int(input())
+print("you were born in:",(a-age))
+
+
